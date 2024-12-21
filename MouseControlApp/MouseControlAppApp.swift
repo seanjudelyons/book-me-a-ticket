51855e99db86cdca6a4809e2c7e65e839394521a
seanjudelyons <seanjudelyons@gmail.com>
2024-12-22 01:35:32 +1100

    Initial Commit

---
+++ MouseControlApp/MouseControlAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseControlAppApp.swift
+//  MouseControlApp
+//
+//  Created by Sean Lyons on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MouseControlAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MouseControlAppTests/MouseControlAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MouseControlAppTests.swift
+//  MouseControlAppTests
+//
+//  Created by Sean Lyons on 22/12/2024.
+//
+
+import Testing
+@testable import MouseControlApp
+
+struct MouseControlAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
